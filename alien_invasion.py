47b2849e3c22008b412bd624a1ee2c27e377f897
Nmbwj <84346757+Nmbwj@users.noreply.github.com>
2025-07-17 02:00:16 +0300

    Refactor alien collision detection and repopulation

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,7 +80,9 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
 
+    def _check_bullet_alien_collisions(self):
         # Chek for any bullets that have hit aliens.
         #   If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(
